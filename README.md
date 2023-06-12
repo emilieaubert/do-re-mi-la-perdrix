140f5c6dc0f7d8ded6a21a612be2e8967f43b1dc
emilieaubert <128042467+emilieaubert@users.noreply.github.com>
2023-06-12 13:53:50 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Ouvrir cette page à [https://emilieaubert.github.io/do-re-mi-la-perdrix/](https://emilieaubert.github.io/do-re-mi-la-perdrix/)
+
+## Utiliser comme extension
+
+Ce dépôt peut être ajouté en tant qu'**extension** dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Nouveau projet**
+* cliquez sur **Extensions** dans le menu engrenage
+* recherchez **https://github.com/emilieaubert/do-re-mi-la-perdrix** et importez
+
+## Éditer ce projet ![Build status badge](https://github.com/emilieaubert/do-re-mi-la-perdrix/workflows/MakeCode/badge.svg)
+
+Éditer ce dépôt dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Importer** puis cliquez sur **Importer l'URL **
+* collez **https://github.com/emilieaubert/do-re-mi-la-perdrix** et cliquez sur importer
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/emilieaubert/do-re-mi-la-perdrix/raw/master/.github/makecode/blocks.png)
+
+#### Métadonnées (utilisées pour la recherche, le rendu)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,33 @@
+music.setTempo(120)
+for (let index = 0; index < 1; index++) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+}
+for (let index = 0; index < 1; index++) {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+}
+for (let index = 0; index < 1; index++) {
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Double))
+}
+for (let index = 0; index < 1; index++) {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+}
